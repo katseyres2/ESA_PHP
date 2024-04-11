219503afa8c9db8f6e40790c478ca685d4077cb7
Maitrepylos <maitrepylos@gmail.com>
2024-04-11 18:44:02 +0200

    Version excellente du prof !

--- damier/index.php
+++ damier/index.php
@@ -6,23 +6,23 @@ $columns = 8;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-	<title>Damier</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Damier</title>
 </head>
 <body>
-	<?php
-	echo '<div class="damier">';
-	for ($i=0; $i<$rows; $i++) {
-		echo '<div class="row">';
-		for ($j=0; $j<$columns; $j++) {
-			$name = ($i + $j) % 2 == 0 ? 'white' : 'black'; 
-			echo '<span class="cell '.$name.'"></span>';
-		}
-		echo '</div>';
-	};
-	echo "</div>";
-	?>
+<div class="damier">
+    <?php for ($i = 0; $i < $rows; $i++): ?>
+        <div class="row">
+            <?php
+            for ($j = 0; $j < $columns; $j++):
+                $name = ($i + $j) % 2 == 0 ? 'white' : 'black';
+                ?>
+                <span class="cell <?= $name ?>"></span>
+            <?php endfor ?>
+        </div>
+    <?php endfor ?>
+</div>
 </body>
 </html>
