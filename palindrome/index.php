fe63f031d951c1dbb6e521a3312c7f637ccacf56
katseyres <143514320+katseyres2@users.noreply.github.com>
2024-04-21 16:15:27 +0200

    Suppression condition inutile.

--- palindrome/index.php
+++ palindrome/index.php
@@ -1,10 +1,8 @@
 <?php
 $word = readline('Enter a word: ');
 
-if ($word) {
-	if (strrev($word) == $word) {
-		echo 'You find a palindrome!';
-	} else {
-		echo 'This is not a palindrome...';
-	}
+if (strrev($word) == $word) {
+	echo 'You find a palindrome!';
+} else {
+	echo 'This is not a palindrome...';
 }
