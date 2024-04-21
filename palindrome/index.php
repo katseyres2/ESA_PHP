70ec633065ea5994ca10715899e97382132a1c0e
Maximilien <maximilien.denis@five.be>
2024-04-21 16:13:27 +0200

    Correction du palindrome.

--- palindrome/index.php
+++ palindrome/index.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html>
-	<body>
-		<form action="/index.php" method="GET">
-			<label for="fname">Word:</label><br>
-			<input type="text" id="word" name="word" value=""><br>
-			<input type="submit" value="Submit">
-		</form>
-		
-		<?php $word = $_GET['word']; ?>
-		<?php if ($word): ?>
-			<div>
-				<div>The word is <?= $word ?></div>
-				<div>
-					<?php if (strrev($word) == $word): ?>
-						You find a palindrome!
-					<?php else: ?>
-						This is not a palindrome...
-					<?php endif ?>
-				</div>
-			</div>
-		<?php endif ?>
-	</body>
-</html>
+<?php
+$word = readline('Enter a word: ');
+
+if ($word) {
+	if (strrev($word) == $word) {
+		echo 'You find a palindrome!';
+	} else {
+		echo 'This is not a palindrome...';
+	}
+}
