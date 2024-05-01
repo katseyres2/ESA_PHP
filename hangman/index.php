a6226b1f3f9b77f39344a0de525ae7aff8a38c7d
Maximilien <maximilien.denis@five.be>
2024-05-01 10:45:44 +0200

    Corrections du pendu.

--- hangman/index.php
+++ hangman/index.php
@@ -6,22 +6,23 @@ $coffeePath = './data/ascii-coffee.txt';
 $words = file('./data/mots.txt');
 
 $rIndex = rand(0, count($words) - 1);
-$rWord = $words[$rIndex];
+$rWord = trim($words[$rIndex]);
 
 $solution = str_repeat('.', strlen($rWord));
 $askedLetters = [];
 $alphabet = range('A', 'Z');
 
-$counter = 8;
+$counter = 16;
 $turn = 0;
 
 while ($counter && str_contains($solution, '.')) {
 	echo str_repeat("-", 40) . "\n";
-	echo dessinPendu($counter) . "\n";
-	echo '> Current word  : ' . $solution . "\n";
-	echo '> Letters asked : ' . join(' ', $askedLetters) . "\n";
+	echo dessinPendu(intdiv($counter, 2)) . "\n";
+	echo '> Remaining trials : ' . $counter . "\n"; 
+	echo '> Current word     : ' . $solution . "\n";
+	echo '> Letters asked    : ' . join(' ', $askedLetters) . "\n";
 
-	$letter = strtoupper(readline("> Select a letter : "));
+	$letter = strtoupper(readline("> Select a letter  : "));
 
 	if (!in_array($letter, $alphabet)) {
 		echo "Unsupported value.\n";
@@ -51,11 +52,11 @@ while ($counter && str_contains($solution, '.')) {
 echo "\n\n";
 
 if ($counter) {
-	$content = file_get_contents('./data/ascii-coffee.txt');
-	echo $content . "\n";
-	echo "\n\t     Good job, you can take a rest...\n\n";
+	// $content = file_get_contents('./data/ascii-coffee.txt');
+	// echo $content . "\n";
+	echo "\n\t     Good job, the word was $rWord. You can take a rest...\n\n";
 } else {
-	$content = file_get_contents('./data/ascii-bart.txt');
-	echo $content . "\n";
-	echo "\n\t     AH AH, You're dead!\n\n";
+	// $content = file_get_contents('./data/ascii-bart.txt');
+	// echo $content . "\n";
+	echo "\n\t     You lose, the word was $rWord\n\n";
 }
